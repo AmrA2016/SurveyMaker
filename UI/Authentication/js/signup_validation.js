296804d2c51b638cb205c35f94ee7ab7a2b61773
AmrA2016 <amr.atef96@hotmail.com>
2017-12-02 23:34:49 +0200

    Edit function call in Signup_form submit event

--- UI/Authentication/js/signup_validation.js
+++ UI/Authentication/js/signup_validation.js
@@ -1,40 +1,41 @@
-function validateemail()
-{
-var x = document.signup_form.email.value;
-var atposition = x.indexOf("@");
-var dotposition = x.lastIndexOf(".");
-if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length){
-  alert("Please enter a valid e-mail address");
-  return false;
-  }
-}
-function matchpass(){
-var firstpassword = document.signup_form.password1.value;
-var secondpassword = document.signup_form.password2.value;
+function validateemail() {
+    var x = document.signup_form.email.value;
+    var atposition = x.indexOf("@");
+    var dotposition = x.lastIndexOf(".");
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+        alert("Please enter a valid e-mail address");
+        return false;
+    }
+};
 
-if(firstpassword == secondpassword){
-return true;
-}
-else{
-alert("password must be same!");
-return false;
-}
-}
+function matchpass() {
+    var firstpassword = document.signup_form.password1.value;
+    var secondpassword = document.signup_form.password2.value;
 
-function validateNumber(){
-var num = document.signup_form.mobile.value;
-if (isNaN(num)){
-  document.getElementById("exampleInputMobile1").innerHTML="Enter Numeric value only";
-  return false;
-}else{
-  return true;
-  }
-}
-function validation(){
+    if (firstpassword == secondpassword) {
+        return true;
+    } else {
+        alert("password must be same!");
+        return false;
+    }
+};
 
-  validateemail();
-  validateNumber();
-  matchpass();
+function validateNumber() {
+
+    var num = document.signup_form.mobile.value;
+    if (isNaN(num)) {
+        document.getElementById("exampleInputMobile1").innerHTML = "Enter Numeric value only";
+        return false;
+    } else {
+        return true;
+    }
+};
+
+function validation() {
+
+    validateemail();
+    validateNumber();
+    matchpass();
 
 
 }
