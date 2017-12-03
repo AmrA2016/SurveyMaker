27246538e65206a7468e0ad50d2e5abe320e2b02
unknown <manarashraf711@gmail.com>
2017-12-03 19:26:22 +0200

    login/signup form validation with issue to be sloved

---
+++ UI/Authentication/js/form_validation.js
@@ -0,0 +1,47 @@
+function validateemail() {
+    var x = document.form.email.value;
+    var atposition = x.indexOf("@");
+    var dotposition = x.lastIndexOf(".");
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+        document.getElementById("emailmsg").style.display="block";
+        return false;
+    }
+    else {
+      document.getElementById("emailmsg").style.display="none";
+      return true;
+    }
+};
+
+function matchpass() {
+    var firstpassword = document.form.password1.value;
+    var secondpassword = document.form.password2.value;
+
+    if (firstpassword == secondpassword) {
+       document.getElementById("passwordmsg").style.display="none"
+        return true;
+    } else {
+        document.getElementById("passwordmsg").style.display="block";
+        return false;
+    }
+};
+
+function validateNumber() {
+
+    var num = document.form.mobile.value;
+    if (isNaN(num)) {
+        document.getElementById("mobilemsg").style.display="block";
+        return false;
+    } else {
+        document.getElementById("mobilemsg").style.display="none";
+        return true;
+    }
+};
+
+function validation() {
+
+    validateemail();
+    validateNumber();
+    matchpass();
+
+
+}
