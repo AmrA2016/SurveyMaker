f99f1779f9c3753d8defa55815b5ca2205adcee0
unknown <manarashraf711@gmail.com>
2017-12-04 16:34:26 +0200

    Login (back-end)

--- SurveyMaker_WebApp/src/java/Servlets/AuthenticationServlet.java
+++ SurveyMaker_WebApp/src/java/Servlets/AuthenticationServlet.java
@@ -10,6 +10,7 @@ import Models.UserModel;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -31,7 +32,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Amr
  */
-@WebServlet(name = "AuthenticationServlet", urlPatterns = {"/Login","/getResetForm","/ResetPassword","/Logout"})
+@WebServlet(name = "AuthenticationServlet", urlPatterns = {"/Login", "/getResetForm", "/ResetPassword", "/Logout"})
 public class AuthenticationServlet extends HttpServlet {
 
     /**
@@ -44,18 +45,19 @@ public class AuthenticationServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException , ClassNotFoundException, SQLException {
-       
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+
         String path = request.getServletPath();
-        
-        if(path.equals("/Login"))
-            login(request,response);
-        else if(path.equals("/getResetForm"))
-            getResetPasswordForm(request,response);
-        else if(path.equals("/ResetPassword"))
-            resetPassword(request,response);
-        else if(path.equals("/Logout"))
-            logout(request,response);
+
+        if (path.equals("/Login")) {
+            login(request, response);
+        } else if (path.equals("/getResetForm")) {
+            getResetPasswordForm(request, response);
+        } else if (path.equals("/ResetPassword")) {
+            resetPassword(request, response);
+        } else if (path.equals("/Logout")) {
+            logout(request, response);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -110,7 +112,38 @@ public class AuthenticationServlet extends HttpServlet {
     }// </editor-fold>
 
     private void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        UserModel user_model = new UserModel();
+        User user = null;
+        user = user_model.getByMail(email);
+
+        if (user == null) {
+            
+            response.sendRedirect("Authentication/invalid_email.jsp");
+
+        } else {
+
+            if (user.getPassword().equals(password)) {
+                if (user.isVerified() == true) {
+                    if (user.isAdmin() == true) {
+                        response.sendRedirect("Admin/admin_home.jsp");
+                    } else {
+                        response.sendRedirect("User/user_home.jsp");
+                    }
+                }else{
+                    
+                    response.sendRedirect("Authentication/confirm_account.jsp");
+                }
+
+            }else{
+                
+                response.sendRedirect("Authentication/invalid_password.jsp");
+                
+            }
+        }
+
     }
 
     private void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -125,35 +158,32 @@ public class AuthenticationServlet extends HttpServlet {
     private void resetPassword(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException, SQLException {
         String mail = request.getParameter("email");
         User user = UserModel.getByMail(mail);
-        if(user == null)
-        {
+        if (user == null) {
             request.setAttribute("InvalidMail", true);
             request.getRequestDispatcher("Authentication/forget_password.jsp").forward(request, response);
-        }
-        else{
+        } else {
             String newPassword = UUID.randomUUID() + "";
-            
+
             UserModel.setPasswrod(user.getId(), newPassword);
-           
+
             String subject = "Survey Maker | Reset Password";
             String content = "This is your new account authentication info\n"
-                            + "Email: " + mail
-                            + "\nPassword: " + newPassword;
-                              
+                    + "Email: " + mail
+                    + "\nPassword: " + newPassword;
+
             sendMail(mail, subject, content);
             response.sendRedirect(request.getContextPath());
         }
     }
 
-    private boolean sendMail(String Reciever, String subject, String text){
-        
+    private boolean sendMail(String Reciever, String subject, String text) {
+
         String username = "surveymaker.owner@gmail.com";
         String password = "iaP@ssw0rd!";
-        
+
         String from = "surveymaker.owner@gmail.com";
 
         String to = Reciever;
-
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
